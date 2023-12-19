47119695a1931966c00ddbbed2fd626e9ff8c3ba
dyanik9 <dyanik9@gmail.com>
2023-12-19 11:15:49 +0100

    super

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_soundgen (
+    tt_um_soundgen test (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
