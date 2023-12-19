e7f9726a5ea31785062283d7c6f79699239e77af
dyanik9 <dyanik9@gmail.com>
2023-12-19 10:57:36 +0100

    hallo

--- src/tb.v
+++ src/tb.v
@@ -1,40 +1,47 @@
+`default_nettype none
+`timescale 1ns/1ps
+
 /*
-2 Simple testbench for the counter .
-3 */
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
 
-‘timescale 1 ns / 1 ns
+// testbench is controlled by test.py
+module tb ();
 
-‘include " tt_um_soundgen . v "
+    // this part dumps the trace to a vcd file that can be viewed with GTKWave
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
+    end
 
-module tb ;
+    // wire up the inputs and outputs
+    reg  clk;
+    reg  rst_n;
+    reg  ena;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
 
-	parameter BW = 3;
+    wire [3:0] counter_out = uo_out[3:0];
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-	 // inputs
-	reg rst_i = 1 ’ b1 ;
-	reg clk_i = 1 ’ b0 ;
-	wire [ BW -1:0] cnt_val ;
+    tt_um_soundgen (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
 
-	// DUT
-	counter
-		#( BW )
-		counter_dut (
-			. clk_i ( clk_i ) ,
-			. rst_i ( rst_i ) ,
-			. counter_val_o ( cnt_val )
-	) ;
-	// Generate clock
-	/* verilator lint_off STMTDLY */
-	always #5 clk_i = ~ clk_i ;
-	/* verilator lint_on STMTDLY */
-
-	initial begin
-		$dumpfile ( " tt_um_soundgen . vcd " ) ;
-		$dumpvars ;
-
-		/* verilator lint_off STMTDLY */
-		#50 rst_i = 1 ’ b0 ; // deassert reset
-		#200 $finish ; // finish
-		/* verilator lint_on STMTDLY */
-	end
-endmodule // tb
+endmodule
--- src/tt_um_soundgen.v
+++ src/tt_um_soundgen.v
@@ -1,38 +1,40 @@
-/*
-2 Simple counter with generic bitwidth .
-3 */
-‘default_nettype none
-‘ifndef __SOUNDGEN__
-‘define __SOUNDGEN__
+`default_nettype none
 
-module tt_um_soundgen
-#(
-	parameter BW = 8 // optional parameter
-) (
-	// define I /O ’ s of the module
-	input clk_i , // clock
-	input rst_i , // reset
-	output wire [ BW -1:0] counter_val_o // counter value
-) ;
-	// start the module implementation
+module tt_um_soundgen (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
-	// register to store the counter value
-	reg [ BW -1:0] counter_val ;
-	// assign the counter value to the output
-	assign counter_val_o = counter_val ;
+    wire reset = ! rst_n;
+    assign uo_out[7:4] = 4'd0;
+    assign uo_out[3:0] = ctr_r;
 
-	always @ ( posedge clk_i ) begin
-		// gets active always when a positive edge of the clock signal occours
+    /* verilator lint_off UNUSEDSIGNAL */
+    wire [7:0] dummy1 = ui_in;
+    wire [7:0] dummy2 = uio_in;
+    wire dummy3 = ena;
+    /* verilator lint_on UNUSEDSIGNAL */
 
-		if ( rst_i == 1 ’ b1 ) begin
-			// if reset is enabled
-			counter_val <= { BW {1 ’ b0 }}; // reset the counter value
-		end else begin
-			// increment the counter value by 1
-			counter_val <= counter_val + {{( BW -1) {1 ’ b0 }} , 1 ’ b1 };
-		end
-	end
+    // use bidirectionals as outputs
+    assign uio_oe = 8'b11111111;
+    assign uio_out = 8'd0;
+
+    // declare local registers
+    reg [3:0] ctr_r;
+
+    // here is the action
+    always @(posedge clk) begin
+        if (reset) begin
+            ctr_r <= 4'd0;
+        end else begin
+		ctr_r <= ctr_r + 1'b1;
+        end
+    end
+
 endmodule // tt_um_soundgen
-
-‘endif
-‘default_nettype wire
