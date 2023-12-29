da07809607dc445910a935740f3d35d986b27bc8
dyanik9 <dyanik9@gmail.com>
2023-12-29 15:14:51 +0100

    first test with 10MHz clk

--- src/prelude.v
+++ src/prelude.v
@@ -17,17 +17,22 @@ module prelude (
 	// loop through sine with f*len(LUT) --> brings one period --> we need 9 Bit counter
 	// we have 10MHz clk
 	
+	/* verilator lint_off UNUSEDPARAM */
 	parameter A = 'd177;		// 220 Hz		--> f = 56320 Hz		--> maxval = 177.5
 	parameter Dhigh = 'd133;	// 293.66 Hz	--> f = 75176.96 Hz		--> maxval = 133
 	parameter C = 'd149;		// 261.62 Hz	--> f = 66974.72 Hz		--> maxval = 149.3
 	parameter B = 'd158;		// 246.94 Hz	--> f = 63216.64 Hz		--> maxval = 158.2
 	parameter G = 'd199;		// 195.99 Hz	--> f = 50173.44 Hz		--> maxval = 199.3
-	parameter Fis = 'd211;	// 184.99 Hz	--> f = 47357.44 Hz		--> maxval = 211.2
+	parameter Fis = 'd211;		// 184.99 Hz	--> f = 47357.44 Hz		--> maxval = 211.2
 	parameter E = 'd237;		// 164.81 Hz	--> f = 42191.36 Hz		--> maxval = 237
 	parameter D = 'd266;		// 146.83 Hz	--> f = 37588.48 Hz		--> maxval = 266
+	/* verilator lint_on UNUSEDPARAM */
 	
-	reg [PITCH_BITWIDTH-1:0] pitch [20];		// melody to play
-	reg [12:0] duration [20];	// duration of each tone
+	//reg [PITCH_BITWIDTH-1:0] pitch [20];		// melody to play
+	//reg [12:0] duration [20];	// duration of each tone
+	
+	localparam [PITCH_BITWIDTH-1:0] pitch [20] = {D, G, G, A, B, G, Dhigh, B, B, C, Dhigh, C, B, C, Dhigh, A, G, A, B, A};	// 20 tones
+	localparam [12:0] duration [20] = {'d4000, 'd4000, 'd2000, 'd2000, 'd4000, 'd4000, 'd8000, 'd6000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000};	// das sind number of samples
 	
 	reg [PITCH_BITWIDTH-1:0] pos_sine;
 	reg [PITCH_BITWIDTH-1:0] neg_sine;
@@ -48,7 +53,7 @@ module prelude (
 		.neg_out(neg_sine)
     );
     
-    parameter DAC_PERIOD = 'd511;	// 10*fmax ~ 3000Hz --> with 10MHz clk --> period = 3333.33
+    parameter DAC_PERIOD = 'd511;	// a full 9Bit PWM (19kHz --> TODO: passt?) (ma kann sicher auf fs runtergehen, wär gscheiter oder?)
 	
 	// DAC pos edge
     dac #(PITCH_BITWIDTH) dac_pos (
@@ -84,7 +89,7 @@ module prelude (
 		.clk_o(clk_sine)
     );
 	
-	// TODO: correct maxval for given frequency? (we have 10MHz clk)
+	// TODO: correct maxval for given frequency? (we have 10MHz clk) --> YES!
 	// TODO: reset clkgen, if frequency changed! (--> directly in clkgen, if maxval changed)
 		
     
@@ -100,9 +105,8 @@ module prelude (
         if (reset) begin
         	ctr_pitch_i <= 'd0;
         	ctr_duration <= 'd0;
-            pitch = '{D, G, G, A, B, G, Dhigh, B, B, C, Dhigh, C, B, C, Dhigh, A, G, A, B, A};	// 20 tones
-            duration = '{'d4000, 'd4000, 'd2000, 'd2000, 'd4000, 'd4000, 'd8000, 'd6000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000};	// das sind number of samples
-            //duration = '{'d4, 'd4, 'd2, 'd2, 'd4, 'd4, 'd8, 'd6, 'd2, 'd4, 'd2, 'd2, 'd2, 'd2, 'd4, 'd2, 'd2, 'd2, 'd2, 'd4};	// das sind number of samples
+            //pitch = '{D, G, G, A, B, G, Dhigh, B, B, C, Dhigh, C, B, C, Dhigh, A, G, A, B, A};	// 20 tones
+            //duration = '{'d4000, 'd4000, 'd2000, 'd2000, 'd4000, 'd4000, 'd8000, 'd6000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000, 'd2000, 'd2000, 'd2000, 'd2000, 'd4000};	// das sind number of samples
         end else if (fs_clk) begin	 // count at every fs
 	    	// program flow to play prelude
 	    	if(ctr_duration >= duration[ctr_pitch_i]-'d1) begin	// tone finished --> select next pitch
